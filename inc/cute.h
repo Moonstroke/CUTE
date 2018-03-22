942f7c7cd52fb73370c48dbf4ce033b5576abeb0
joH1 <jiema@live.fr>
2018-03-22 19:34:05 +0100

    Add doc, #1
    The headers `cute.h` ans `cute_assert.h` are documented.
    The other will follow shortly.

--- inc/cute.h
+++ inc/cute.h
@@ -1,24 +1,71 @@
+/**
+ * \file cute.h
+ * \author joH1
+ * \version 0.1
+ *
+ * \brief The main header of the project.
+ *
+ * This file serves two purposes: it contains common features used by the files
+ * of the project (like function attributes), and it can be used to include the
+ * whole project at once.
+ */
 #ifndef CUTE_H
 #define CUTE_H
 
 
 #ifndef __GNUC__
+/**
+ * \brief Empty declaration for non-GNU C.
+ */
 #define __attribute__(x)
 #endif
 
+/**
+ * \brief The function is inlined everywhere it is met.
+ */
 #define CUTE_INLINE __attribute__((__always_inline__)) inline
 
+/**
+ * \brief The function does not affect external data.
+ */
 #define CUTE_PURE __attribute__((__pure__, __warn_unused_result__))
 
+/**
+ * \brief The arguments to the function given here must not be \c NULL.
+ *
+ * \param[in] ... The indices of the arguments that must not be \c NULL.
+ */
 #define CUTE_NOTNULL(...) __attribute__((__nonnull__(__VA_ARGS__)))
 
-
+/**
+ * \brief The argument at given index, counting backwards, must be \c NULL.
+ *
+ * \note This attribute is only to be used on variadic functions.
+ *
+ * \param i The index
+ */
 #define CUTE_NULL_GUARD(i) __attribute__((__sentinel__(i)))
+
+/**
+ * \brief The last argument of the function \i must be an explicit \c NULL.
+ *
+ * \note As for \a CUTE_NULL_GUARD, this attribute is only valid on variadic
+ *       functions.
+ */
 #define CUTE_LAST_ARG_NULL CUTE_NULL_GUARD(0)
 
-
+/**
+ * \brief The function allocates memory on the heap.
+ */
 #define CUTE_CTOR __attribute__((__malloc__))
 
+/**
+ * \brief The function is related to a OO-like data type.
+ *
+ * \note The meaning of this attribute is purely semantic; it actually only
+ *       ensures that the first argument, the type instance pointer, is not
+ *       \c NULL.
+ */
 #define CUTE_MEMBER CUTE_NOTNULL(1)
 
 
--- inc/cute_assert.h
+++ inc/cute_assert.h
@@ -1,3 +1,15 @@
+/**
+ * \file cute_assert.h
+ * \author joH1
+ * \version 0.1
+ *
+ * \brief Assertions macros.
+ *
+ * This file defines macros to ensure that a condition is \c true.
+ * If the condition is \c false, the program execution ends with an \a abort
+ * call.
+ * If the macro \c NDEBUG is defined, assertions are not executed.
+ */
 #ifndef CUTE_ASSERT_H
 #define CUTE_ASSERT_H
 
@@ -9,15 +21,49 @@
 
 
 
+/**
+ * \brief Aborts if the condition evaluates to \c 0.
+ *
+ * \note This merely is a redefinition of the standard macro \a assert.
+ *
+ * \param[in] cond The expression to evaluate
+ */
 #define CUTE_runTimeAssert(cond) assert(cond)
+
+/**
+ * \brief Evaluates the expression during compilation.
+ *
+ * \param[in] cond The expression to evaluates
+ * \param[in] msg The message to output if the evaluation fails
+ */
 #define CUTE_compileTimeAssert(cond, msg) static_assert(cond, msg)
 
 
+/**
+ * \brief Ensures that the given values are equal.
+ *
+ * \param[in] x The first value
+ * \param[in] y The second value
+ */
 #define CUTE_assertEquals(x, y) CUTE_runTimeAssert((x) == (y))
 
+/**
+ * \brief Ensures that the given values compare to a result value with the given
+ *        function.
+ * \param[in] x    The first value to test
+ * \param[in] y    The second value
+ * \param[in] func The function to compare with
+ * \param[in] ok   The expected return value
+ */
 #define CUTE_assertEqualsUsing(x, y, func, ok) \
 	CUTE_runTimeAssert(func((x), (y)) == (ok))
 
+/**
+ * \brief Compares two string values.
+ *
+ * \param[in] s1 The first string
+ * \param[in] s2 The second string
+ */
 #define CUTE_assertStringEquals(s1, s2) \
 	CUTE_assertEqualsWith((s1), (s2), strcmp, 0)
 
