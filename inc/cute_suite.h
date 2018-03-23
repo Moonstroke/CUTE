65d8707fa04273422b67abc1178a63bb18f5aac4
joH1 <jiema@live.fr>
2018-03-23 18:17:47 +0100

    Rename function parameter
    The parameter `s` of `CUTE_destroyTestSuite` is renamed to `suite`.

--- inc/cute_suite.h
+++ inc/cute_suite.h
@@ -41,9 +41,9 @@ CUTE_CTOR CUTE_TestSuite *CUTE_buildTestSuite(unsigned int size, ...);
  *
  * \note The test cases included in the suite are also freed.
  *
- * \param[in] s The test suite
+ * \param[in] suite The test suite
  */
-void CUTE_destroyTestSuite(CUTE_TestSuite *s);
+void CUTE_destroyTestSuite(CUTE_TestSuite *suite);
 
 /**
  * \brief Runs the test suite.
